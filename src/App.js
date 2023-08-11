b817f32ff3a962419413e923be94a5ea82deecb0
LakshikaMadhushan <lakshikamadhushan3@gmail.com>
2023-08-11 18:59:56 +0530

    login design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import React from 'react'
 import './App.css';
 import Form from './components/Form';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './views/login'
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/dashboard",
+    element: <div>dashboard</div>
+  }
+])
 
 function App() {
-  return (
-      <div className="App">
-        <Form></Form>
-      </div>
-  );
+  return (<React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>);
 }
 
 export default App;
