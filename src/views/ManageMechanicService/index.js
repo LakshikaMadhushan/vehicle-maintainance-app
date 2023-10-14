bec364d9c7c9d89a7d7b7337ce13d16b5cb489a3
LakshikaMadhushan <lakshikamadhushan3@gmail.com>
2023-10-14 21:03:19 +0530

    ui update

--- src/views/ManageMechanicService/index.js
+++ src/views/ManageMechanicService/index.js
@@ -4,6 +4,7 @@ import '../common/style.css'
 import {useNavigate} from 'react-router-dom'
 import {Button, Col, FormGroup, Input, Label, Row} from "reactstrap";
 import Select from 'react-select';
+import DataTable from "react-data-table-component";
 
 
 const options = [
@@ -11,15 +12,65 @@ const options = [
     {value: 'option2', label: 'Option 2'},
     {value: 'option3', label: 'Option 3'}
 ];
-const data = [
-    {id: 1, serviceType: 'Service', category: "SUV", name: "clean radiator", price: "2500"},
-    {id: 2, serviceType: 'Item', category: "Hybrid", name: "Battery", price: "5000000"},
-    {id: 2, serviceType: 'Item', category: "Hybrid", name: "Battery", price: "5000000"},
-    {id: 2, serviceType: 'Item', category: "Hybrid", name: "Battery", price: "5000000"}
-
+const columns = [
+    {
+        name: 'ID',
+        selector: row => row.itemId,
+    },
+    {
+        name: 'Item Name',
+        selector: row => row.itemName,
+    },
+    {
+        name: 'Selling Price',
+        selector: row => row.sellingPrice,
+    },
+    {
+        name: 'Buying Price',
+        selector: row => row.buyingPrice,
+    },
+    {
+        name: 'Brand',
+        selector: row => row.brand,
+    },
+    {
+        name: 'Category Name',
+        selector: row => row.categoryName,
+    },
+    {
+        name: 'Quantity',
+        selector: row => row.quantity,
+    },
+    {
+        name: 'Item Status',
+        selector: row => row.itemStatus,
+    },
 ];
+
+
+const customStyles = {
+    headCells: {
+        style: {
+            backgroundColor: '#F0F0F0',
+            fontWeight:'bold'
+        },
+    }
+};
 const ManageMechanicService = () => {
     const navigate = useNavigate()
+    const data = [
+        {
+            id: 1,
+            title: 'Beetlejuice',
+            year: '1988',
+        },
+        {
+            id: 2,
+            title: 'Ghostbusters',
+            year: '1984',
+        }
+    ]
+
     return <div>
         <Row style={{alignItems: 'center', margin: 0, padding: 0, backgroundColor: "#F1F0E8"}}>
             <Row style={{alignItems: 'center', margin: '0%', height: '80vh', padding: 10, backgroundColor: "#ffffff"}}>
@@ -34,15 +85,15 @@ const ManageMechanicService = () => {
                 }}>
 
                     <Col md={3} align="left">
-                        <FormGroup className="text-field">
+                        <FormGroup className="text-field-mechanic">
                             <Label>Mechanic Service Name</Label>
-                            <Input placeholder=""/>
+                            <Input className="input-field-mechanic" placeholder=""/>
                         </FormGroup>
                     </Col>
                     <Col md={3} align="left">
                         <FormGroup className="text-field">
                             <Label>Price</Label>
-                            <Input placeholder=""/>
+                            <Input className="input-field-mechanic" placeholder=""/>
                         </FormGroup>
                     </Col>
                     <Col md={3} align="left">
@@ -84,9 +135,9 @@ const ManageMechanicService = () => {
                 }}>
 
                     <Col md={3} align="left" style={{padding: 0}}>
-                        <FormGroup className="text-field">
+                        <FormGroup className="text-field-mechanic">
                             <Label>Mechanic Service Name</Label>
-                            <Input placeholder=""/>
+                            <Input className="input-field-mechanic" placeholder=""/>
                         </FormGroup>
                     </Col>
                     <Col md={3} align="left">
@@ -99,51 +150,35 @@ const ManageMechanicService = () => {
                     </Col>
 
                     <Col md={3} align="right">
-                        <Button color="danger" style={{width: '30vh', marginLeft: "0"}}
+                        <Button color="danger" style={{width: '30vh', marginLeft: "0",marginTop:"10px"}}
                                 onClick={() => navigate("/register")}>Clear</Button>
                     </Col>
                     <Col md={3} align="right">
-                        <Button color="success" style={{width: '30vh', marginLeft: "0"}}
+                        <Button color="success" style={{width: '30vh', marginLeft: "0",marginTop:"10px"}}
                                 onClick={() => navigate("/register")}>Filter</Button>
                     </Col>
 
                     <Row style={{
-                        alignItems: 'center', border: '2px solid #ccc', margin: '0%',
-                        borderRadius: '5px', backgroundColor: "yellow", padding: "0px"
+                        alignItems: 'center',
+                        margin: '0%',
+                        height: '50%',
+                        backgroundColor: "yellow",
+                        padding:0,
+                        paddingTop:"2px"
                     }}>
-
-                        <Col md={12} align="right" style={{padding: 0}}>
-                            <table>
-                                <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Item Name</th>
-                                    <th>Brand</th>
-                                    <th>Seller Name</th>
-                                    <th>Buying price</th>
-                                    <th>Selling Price</th>
-                                    <th>Status</th>
-                                    <th>Qty</th>
-                                    <th>Category</th>
-                                    <th>Action</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                {data.map((item) => (
-                                    <tr key={item.id}>
-                                        <td>{item.id}</td>
-                                        <td>{item.serviceType}</td>
-                                        <td>{item.category}</td>
-                                        <td>{item.name}</td>
-                                        <td>{item.price}</td>
-                                    </tr>
-                                ))}
-                                </tbody>
-                            </table>
-
+                        <Col md={12} style={{padding:0,margin:0}} >
+                            <DataTable
+                                columns={columns}
+                                data={data}
+                                pagination
+                                customStyles={customStyles}
+                                paginationRowsPerPageOptions={[3, 5, 10]}
+                                // defaultPageSize={2}
+                                paginationPerPage={3}
+                            />
+                            {}
                         </Col>
                     </Row>
-
                 </Row>
 
 
