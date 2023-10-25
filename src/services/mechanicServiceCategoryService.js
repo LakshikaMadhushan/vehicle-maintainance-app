85aeefa20addc3f81175767f3f0dbbb25f6ca7d8
LakshikaMadhushan <lakshikamadhushan3@gmail.com>
2023-10-25 22:41:26 +0530

    ui update

---
+++ src/services/mechanicServiceCategoryService.js
@@ -0,0 +1,19 @@
+import {apiService} from "./apiConfig";
+
+export const getAllMechanicServiceCategory = async () => {
+    const apiObject = {}
+    apiObject.isBaseUrl = true
+    apiObject.url = `mechanic-service-category`
+    apiObject.method = 'get'
+    // apiObject.body = data
+    return await apiService(apiObject)
+}
+
+export const getAllMechanicServiceCategoryFilter = async (data) => {
+    const apiObject = {}
+    apiObject.isBaseUrl = true
+    apiObject.url = `mechanic-service-category/filter`
+    apiObject.method = 'post'
+    apiObject.body = data
+    return await apiService(apiObject)
+}
--- src/views/manageMechanicServiceCategory/index.js
+++ src/views/manageMechanicServiceCategory/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './style.css'
 import '../common/style.css'
 import { useNavigate } from 'react-router-dom'
@@ -6,35 +6,24 @@ import { Button, Col, FormGroup, Input, Label, Row } from "reactstrap";
 import logo from '../../assets/logo.png'
 import Select from 'react-select';
 import DataTable from 'react-data-table-component';
+import {getAllAdmin} from "../../services/adminService";
+import {
+    getAllMechanicServiceCategory,
+    getAllMechanicServiceCategoryFilter
+} from "../../services/mechanicServiceCategoryService";
+import {getAllTechnician} from "../../services/technicianService";
 
 const columns = [
     {
         name: 'ID',
-        selector: row => row.title,
+        selector: row => row.mechanicServiceCategoryId,
     },
     {
-        name: 'Service Type',
-        selector: row => row.year,
-    },
-    {
-        name: 'Category',
-        selector: row => row.year,
-    },
-    {
-        name: 'Name',
-        selector: row => row.year,
-    },
-    {
-        name: 'Price',
-        selector: row => row.year,
-    },
+        name: 'Category Name',
+        selector: row => row.name,
+    }
 ];
 
-const options = [
-    { value: 'option1', label: 'Option 1' },
-    { value: 'option2', label: 'Option 2' },
-    { value: 'option3', label: 'Option 3' }
-];
 // const data = [
 //     { id: 1, serviceType: 'Service', category: "SUV", name: "clean radiator", price: "2500" },
 //     { id: 2, serviceType: 'Item', category: "Hybrid", name: "Battery", price: "5000000" },
@@ -51,50 +40,45 @@ const customStyles = {
     }
 };
 
+const initialFilterState = {
+    categoryName: null
+}
+
 const ManageItemCategory = () => {
     const navigate = useNavigate()
+    const [filter, setFilter] = useState(initialFilterState)
+    const [tableData, setTableData] = useState([])
+    const [category,setCategory]=useState([])
 
-    const data = [
-        {
-            id: 1,
-            title: 'Beetlejuice',
-            year: '1988',
-        },
-        {
-            id: 2,
-            title: 'Ghostbusters',
-            year: '1984',
-        },
-        {
-            id: 2,
-            title: 'Ghostbusters',
-            year: '1984',
-        },
-        {
-            id: 2,
-            title: 'Ghostbusters',
-            year: '1984',
-        },
-        {
-            id: 2,
-            title: 'Ghostbusters',
-            year: '1984',
-        },
-        {
-            id: 2,
-            title: 'Ghostbusters',
-            year: '1984',
-        },
-        {
-            id: 2,
-            title: 'Ghostbusters',
-            year: '1984',
+
+    useEffect(()=>{
+        onFilter();
+        getAllServiceCategory();
+    },[])
+
+    const getAllServiceCategory=async ()=>{
+        const res= await getAllMechanicServiceCategory()
+        setCategory(res.body.map(category=>{
+            return {
+                label:category.name,
+                value:category.mechanicServiceCategoryId
+            }
+        }))
+    }
+
+    const onFilter = async () => {
+        const body = {
+            name: filter?.categoryName ? filter.categoryName.value : null,
         }
-    ]
+        const response=await getAllMechanicServiceCategoryFilter(body)
+        // setFilter(response.body);
+        setTableData(response.body);
+        // console.log(response);
+    }
 
     return  <div>
         <Row style={{alignItems: 'center', margin: 0, padding: 0, backgroundColor: "#F1F0E8"}}>
-            <Row style={{alignItems: 'center', margin: '0%', padding: 10, backgroundColor: "#ffffff"}}>
+            <Row style={{alignItems: 'center', margin: '0%',height:'80vh', padding: 10, backgroundColor: "#ffffff"}}>
                 <Col md={12} align="left" style={{padding: 0}}>
                     <Label className="heading-text">Manage Service Category</Label>
                     <div className="line"></div>
@@ -112,7 +96,7 @@ const ManageItemCategory = () => {
                         </FormGroup>
                     </Col>
 
-                    <Col md={3} align="right">
+                    <Col md={6} align="right">
                         <Button color="danger" style={{width: '30vh', margin: 0}}
                                 onClick={() => navigate("/register")}>Clear</Button>
                     </Col>
@@ -136,28 +120,28 @@ const ManageItemCategory = () => {
                     backgroundColor: "yellow"
                 }}>
 
-                    <Col md={3} align="left" style={{padding: 0}}>
-                        <FormGroup className="text-field-mechanic">
-                            <Label>Mechanic Service Name</Label>
-                            <Input className="input-field-mechanic" placeholder=""/>
-                        </FormGroup>
-                    </Col>
-                    <Col md={3} align="left">
+
+                    <Col md={6} align="left">
                         <FormGroup className="text-field">
                             <Label>Category</Label>
                             <div className="modern-dropdown">
-                                <Select options={options}/>
+                                <Select options={category} value={filter.categoryName} onChange={(e) => {
+                                    setFilter({...filter, categoryName: e})
+                                }}/>
                             </div>
                         </FormGroup>
                     </Col>
 
                     <Col md={3} align="right">
                         <Button color="danger" style={{width: '30vh', marginLeft: "0", marginTop: "10px"}}
-                                onClick={() => navigate("/register")}>Clear</Button>
+                                onClick={() => {
+                                    setFilter(initialFilterState);
+                                    onFilter();
+                                }}>Clear</Button>
                     </Col>
                     <Col md={3} align="right">
                         <Button color="success" style={{width: '30vh', marginLeft: "0", marginTop: "10px"}}
-                                onClick={() => navigate("/register")}>Filter</Button>
+                                onClick={onFilter}>Filter</Button>
                     </Col>
 
                     <Row style={{
@@ -171,12 +155,12 @@ const ManageItemCategory = () => {
                         <Col md={12} style={{padding: 0, margin: 0}}>
                             <DataTable
                                 columns={columns}
-                                data={data}
+                                data={tableData}
                                 pagination
                                 customStyles={customStyles}
-                                paginationRowsPerPageOptions={[5, 10, 15]}
+                                paginationRowsPerPageOptions={[3,5, 10]}
                                 // defaultPageSize={2}
-                                paginationPerPage={5}
+                                paginationPerPage={3}
                             />
                             {}
                         </Col>
