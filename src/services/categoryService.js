f92e26186a65552af92b46980c65c603e59201aa
LakshikaMadhushan <lakshikamadhushan3@gmail.com>
2023-11-07 18:24:07 +0530

    update service

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -9,3 +9,29 @@ export const getAllCategory = async () => {
     // apiObject.body = data
     return await apiService(apiObject)
 }
+export const getAllItemCategoryFilter = async (data) => {
+    const apiObject = {}
+    apiObject.isBaseUrl = true
+    apiObject.url = `category/filter`
+    apiObject.method = 'post'
+    apiObject.body = data
+    return await apiService(apiObject)
+}
+
+export const saveItemCategory = async (data) => {
+    const apiObject = {}
+    apiObject.isBaseUrl = true
+    apiObject.url = `category`
+    apiObject.method = 'post'
+    apiObject.body = data
+    return await apiService(apiObject)
+}
+
+export const updateItemCategory = async (data) => {
+    const apiObject = {}
+    apiObject.isBaseUrl = true
+    apiObject.url = `category`
+    apiObject.method = 'put'
+    apiObject.body = data
+    return await apiService(apiObject)
+}
--- src/views/manageItemCategory/index.js
+++ src/views/manageItemCategory/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './style.css'
 import '../common/style.css'
 import { useNavigate } from 'react-router-dom'
@@ -6,41 +6,32 @@ import { Button, Col, FormGroup, Input, Label, Row } from "reactstrap";
 import logo from '../../assets/logo.png'
 import Select from 'react-select';
 import DataTable from 'react-data-table-component';
+import {
+    getAllMechanicServiceCategory,
+    getAllMechanicServiceCategoryFilter, saveMechanicServiceCategory, updateMechanicServiceCategory
+} from "../../services/mechanicServiceCategoryService";
+import {
+    getAllCategory,
+    getAllItemCategoryFilter,
+    saveItemCategory,
+    updateItemCategory
+} from "../../services/categoryService";
 
-const columns = [
+const columns = (onEdit) => [
     {
         name: 'ID',
-        selector: row => row.title,
+        selector: row => row.categoryId,
     },
     {
-        name: 'Service Type',
-        selector: row => row.year,
+        name: 'Item Category Name',
+        selector: row => row.categoryName,
     },
     {
-        name: 'Category',
-        selector: row => row.year,
-    },
-    {
-        name: 'Name',
-        selector: row => row.year,
-    },
-    {
-        name: 'Price',
-        selector: row => row.year,
-    },
+        name: 'Action',
+        selector: row => <Button onClick={() => onEdit(row)} color={"success"}>Edit</Button>,
+    }
 ];
 
-const options = [
-    { value: 'option1', label: 'Option 1' },
-    { value: 'option2', label: 'Option 2' },
-    { value: 'option3', label: 'Option 3' }
-];
-// const data = [
-//     { id: 1, serviceType: 'Service', category: "SUV", name: "clean radiator", price: "2500" },
-//     { id: 2, serviceType: 'Item', category: "Hybrid", name: "Battery", price: "5000000" },
-//     { id: 3, serviceType: 'Item', category: "Hybrid", name: "Battery", price: "5000000" }
-//
-// ];
 
 const customStyles = {
     headCells: {
@@ -50,51 +41,85 @@ const customStyles = {
         },
     }
 };
+const initialFilterState = {
+    categoryName: null
+}
 
+const initialFormState = {
+    ItemCategoryName: ""
+}
 const ManageMechanicServiceCategory = () => {
     const navigate = useNavigate()
+    const [filter, setFilter] = useState(initialFilterState)
+    const [tableData, setTableData] = useState([])
+    const [category,setCategory]=useState([])
+    const [formData, setFormData] = useState(initialFormState)
 
-    const data = [
-        {
-            id: 1,
-            title: 'Beetlejuice',
-            year: '1988',
-        },
-        {
-            id: 2,
-            title: 'Ghostbusters',
-            year: '1984',
-        },
-        {
-            id: 2,
-            title: 'Ghostbusters',
-            year: '1984',
-        },
-        {
-            id: 2,
-            title: 'Ghostbusters',
-            year: '1984',
-        },
-        {
-            id: 2,
-            title: 'Ghostbusters',
-            year: '1984',
-        },
-        {
-            id: 2,
-            title: 'Ghostbusters',
-            year: '1984',
-        },
-        {
-            id: 2,
-            title: 'Ghostbusters',
-            year: '1984',
+
+    useEffect(()=>{
+        onFilter(true);
+        getAllItemCategory();
+    },[])
+
+    const getAllItemCategory=async ()=>{
+        const res= await getAllCategory()
+        setCategory(res.body.map(category=>{
+            return {
+                label:category.categoryName,
+                value:category.categoryId
+            }
+        }))
+    }
+
+    const onFilter = async (data) => {
+        const tempBody = data ? {...initialFilterState} : filter
+        const body = {
+            name: tempBody?.categoryName ? tempBody.categoryName.value : null,
         }
-    ]
+        const response=await getAllItemCategoryFilter(body)
+        // setFilter(response.body);
+        setTableData(response.body);
+        // console.log(response);
+    }
+
+    const onChangeHandler = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+
+    const itemCategorySave = async () => {
+        const body = {
+            name: formData?.ItemCategoryName,
+        }
+        if (formData?.categoryId) {
+            body.categoryId = formData.categoryId
+            await updateItemCategory(body)
+        } else {
+            await saveItemCategory(body)
+        }
+
+        console.log(body)
+
+        // console.log(formData)
+    }
+
+
+    const itemCategoryEdit = async (row) => {
+        setFormData(
+            {
+                categoryId: row.categoryId,
+                ItemCategoryName: row.categoryName
+            }
+        )
+    }
+
 
     return <div>
         <Row style={{alignItems: 'center', margin: 0, padding: 0, backgroundColor: "#F1F0E8"}}>
-            <Row style={{alignItems: 'center', margin: '0%', padding: 10, backgroundColor: "#ffffff"}}>
+            <Row style={{alignItems: 'center', margin: '0%', padding: 0, backgroundColor: "#ffffff"}}>
                 <Col md={12} align="left" style={{padding: 0}}>
                     <Label className="heading-text">Manage Item Category</Label>
                     <div className="line"></div>
@@ -108,18 +133,19 @@ const ManageMechanicServiceCategory = () => {
                     <Col md={3} align="left">
                         <FormGroup className="text-field-mechanic">
                             <Label>Item Category Name</Label>
-                            <Input className="input-field-mechanic" placeholder=""/>
+                            <Input className="input-field-mechanic" value={formData.ItemCategoryName}
+                                   name={"mechanicCategoryName"} onChange={onChangeHandler}/>
                         </FormGroup>
                     </Col>
 
 
                     <Col md={3} align="right">
                         <Button color="danger" style={{width: '30vh', margin: 0}}
-                                onClick={() => navigate("/register")}>Clear</Button>
+                                onClick={() => setFormData({...initialFormState})}>Clear</Button>
                     </Col>
                     <Col md={3} align="right">
-                        <Button color="success" style={{width: '30vh', margin: 0}}
-                                onClick={() => navigate("/register")}>Save</Button>
+                        <Button color={formData?.categoryId ? "warning" : "success"} style={{width: '30vh', marginLeft: "15px"}}
+                                onClick={itemCategorySave}>{formData?.categoryId ? 'Update' : 'Save'}</Button>
                     </Col>
                 </Row>
 
@@ -127,24 +153,32 @@ const ManageMechanicServiceCategory = () => {
                     alignItems: 'center',
                     margin: '0%',
                     padding: '0%',
-                    backgroundColor: "yellow"
+                    backgroundColor: "yellow",
+                    borderRadius: '5px'
                 }}>
 
-                    <Col md={3} align="left" style={{padding: 0}}>
-                        <FormGroup className="text-field-mechanic">
-                            <Label>Item Category Name</Label>
-                            <Input className="input-field-mechanic" placeholder=""/>
+                    <Col md={3} align="left">
+                        <FormGroup className="text-field">
+                            <Label>Category</Label>
+                            <div className="modern-dropdown">
+                                <Select options={category} value={filter.categoryName} onChange={(e) => {
+                                    setFilter({...filter, categoryName: e})
+                                }}/>
+                            </div>
                         </FormGroup>
                     </Col>
 
 
                     <Col md={3} align="right">
                         <Button color="danger" style={{width: '30vh', marginLeft: "0", marginTop: "10px"}}
-                                onClick={() => navigate("/register")}>Clear</Button>
+                                onClick={async () => {
+                                    await setFilter({...initialFilterState});
+                                    await onFilter(true);
+                                }}>Clear</Button>
                     </Col>
                     <Col md={3} align="right">
                         <Button color="success" style={{width: '30vh', marginLeft: "0", marginTop: "10px"}}
-                                onClick={() => navigate("/register")}>Filter</Button>
+                                onClick={()=>onFilter(false)}>Filter</Button>
                     </Col>
 
                     <Row style={{
@@ -157,8 +191,8 @@ const ManageMechanicServiceCategory = () => {
                     }}>
                         <Col md={12} style={{padding: 0, margin: 0}}>
                             <DataTable
-                                columns={columns}
-                                data={data}
+                                columns={columns(itemCategoryEdit)}
+                                data={tableData}
                                 pagination
                                 customStyles={customStyles}
                                 paginationRowsPerPageOptions={[ 5, 10]}
