252db7b0f51fe52b4b361b4a0070da298147b6de
LakshikaMadhushan <lakshikamadhushan3@gmail.com>
2023-11-07 16:37:25 +0530

    update service

--- src/services/mechanicServiceService.js
+++ src/services/mechanicServiceService.js
@@ -9,3 +9,21 @@ export const getAllMechanicServiceFilter = async (data) => {
     apiObject.body = data
     return await apiService(apiObject)
 }
+
+export const saveMechanicService = async (data) => {
+    const apiObject = {}
+    apiObject.isBaseUrl = true
+    apiObject.url = `mechanic-service`
+    apiObject.method = 'post'
+    apiObject.body = data
+    return await apiService(apiObject)
+}
+
+export const updateMechanicService = async (data) => {
+    const apiObject = {}
+    apiObject.isBaseUrl = true
+    apiObject.url = `mechanic-service`
+    apiObject.method = 'put'
+    apiObject.body = data
+    return await apiService(apiObject)
+}
--- src/views/ManageMechanicService/index.js
+++ src/views/ManageMechanicService/index.js
@@ -6,8 +6,15 @@ import {Button, Col, FormGroup, Input, Label, Row} from "reactstrap";
 import Select from 'react-select';
 import DataTable from "react-data-table-component";
 import {getAllFilterTechnician} from "../../services/technicianService";
-import {getAllMechanicServiceFilter} from "../../services/mechanicServiceService";
-import {getAllMechanicServiceCategory} from "../../services/mechanicServiceCategoryService";
+import {
+    getAllMechanicServiceFilter,
+    saveMechanicService,
+    updateMechanicService
+} from "../../services/mechanicServiceService";
+import {
+    getAllMechanicServiceCategory, saveMechanicServiceCategory,
+    updateMechanicServiceCategory
+} from "../../services/mechanicServiceCategoryService";
 
 
 const options = [
@@ -16,7 +23,7 @@ const options = [
     {value: 'HYBRID', label: 'HYBRID'},
     {value: 'MINI', label: 'MINI'}
 ];
-const columns = [
+const columns = (onEdit) => [
     {
         name: 'ID',
         selector: row => row.mechanicServiceId,
@@ -36,6 +43,10 @@ const columns = [
     {
         name: 'Service Category Name',
         selector: row => row.mechanicServiceCategoryName,
+    },
+    {
+        name: 'Action',
+        selector: row => <Button onClick={() => onEdit(row)} color={"success"}>Edit</Button>,
     }
 ];
 
@@ -55,11 +66,19 @@ const initialFilterState = {
     mechanicServiceCategory: null
 }
 
+const initialFormState = {
+    mechanicServiceFormName: "",
+    mechanicServicePrice: "",
+    mechanicServiceType: null,
+    mechanicServiceCategory: null
+}
+
 const ManageMechanicService = () => {
     const navigate = useNavigate()
     const [filter, setFilter] = useState(initialFilterState)
     const [tableData, setTableData] = useState([])
     const [category,setCategory]=useState([])
+    const [formData, setFormData] = useState(initialFormState)
 
     useEffect(()=>{
         onFilter();
@@ -76,11 +95,12 @@ const ManageMechanicService = () => {
         }))
     }
 
-    const onFilter = async () => {
+    const onFilter = async (data) => {
+        const tempBody = data ? {...initialFilterState} : filter
         const body = {
-            mechanicServiceCategory: filter?.mechanicServiceCategory ? filter.mechanicServiceCategory.value : null,
-            name: filter?.mechanicServiceName ? filter.mechanicServiceName : null,
-            vehicleType: filter?.vehicleType ? filter.vehicleType.value : null,
+            mechanicServiceCategory: tempBody?.mechanicServiceCategory ? tempBody.mechanicServiceCategory.value : null,
+            name: tempBody?.mechanicServiceName ? tempBody.mechanicServiceName : null,
+            vehicleType: tempBody?.vehicleType ? tempBody.vehicleType.value : null,
 
         }
         const response=await getAllMechanicServiceFilter(body)
@@ -89,6 +109,45 @@ const ManageMechanicService = () => {
         // console.log(response);
     }
 
+    const onChangeHandler = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const mechanicServiceSave = async () => {
+        const body = {
+            mechanicServiceCategoryId: formData?.mechanicServiceCategory.value,
+            name: formData?.mechanicServiceFormName,
+            price: formData?.mechanicServicePrice,
+            vehicleType: formData?.mechanicServiceType,
+
+        }
+        if (formData?.mechanicServiceId) {
+            body.mechanicServiceId = formData.mechanicServiceId
+            await updateMechanicService(body)
+        } else {
+            await saveMechanicService(body)
+        }
+
+        console.log(body)
+
+        // console.log(formData)
+    }
+
+
+    const mechanicServiceEdit = async (row) => {
+        setFormData(
+            {
+                mechanicServiceId: row.mechanicServiceId,
+                mechanicServiceFormName: row.name,
+                mechanicServicePrice: row.price,
+                mechanicServiceType: {label: row.vehicleType, value: row.vehicleType},
+                mechanicServiceCategory:{label: row.mechanicServiceCategoryName, value: row.mechanicServiceCategoryId}
+            }
+        )
+    }
 
     return <div>
         <Row style={{alignItems: 'center', margin: 0, padding: 0, backgroundColor: "#F1F0E8"}}>
@@ -106,20 +165,28 @@ const ManageMechanicService = () => {
                     <Col md={3} align="left">
                         <FormGroup className="text-field-mechanic">
                             <Label>Mechanic Service Name</Label>
-                            <Input className="input-field-mechanic" placeholder=""/>
+                            <Input className="input-field-mechanic" value={formData.mechanicServiceFormName}
+                                   name={"mechanicServiceFormName"} onChange={onChangeHandler}/>
                         </FormGroup>
                     </Col>
                     <Col md={3} align="left">
                         <FormGroup className="text-field">
                             <Label>Price</Label>
-                            <Input className="input-field-mechanic" placeholder=""/>
+                            <Input className="input-field-mechanic" value={formData.mechanicServicePrice}
+                                   name={"mechanicServicePrice"} onChange={onChangeHandler}/>
                         </FormGroup>
                     </Col>
                     <Col md={3} align="left">
                         <FormGroup className="text-field">
                             <Label>Vehicle Type</Label>
                             <div className="modern-dropdown">
-                                <Select options={options}/>
+                                <Select options={options} value={formData.mechanicServiceType}
+                                        onChange={(e) => onChangeHandler({
+                                            target: {
+                                                name: 'mechanicServiceType',
+                                                value: e
+                                            }
+                                        })}/>
                             </div>
                         </FormGroup>
                     </Col>
@@ -127,7 +194,13 @@ const ManageMechanicService = () => {
                         <FormGroup className="text-field">
                             <Label>Category</Label>
                             <div className="modern-dropdown">
-                                <Select options={options}/>
+                                <Select options={category} value={formData.mechanicServiceCategory}
+                                        onChange={(e) => onChangeHandler({
+                                            target: {
+                                                name: 'mechanicServiceCategory',
+                                                value: e
+                                            }
+                                        })}/>
                             </div>
                         </FormGroup>
                     </Col>
@@ -138,11 +211,11 @@ const ManageMechanicService = () => {
                 }}>
                     <Col md={3} align="right">
                         <Button color="danger" style={{width: '30vh', margin: 0}}
-                                onClick={() => navigate("/register")}>Clear</Button>
+                                onClick={() => setFormData({...initialFormState})}>Clear</Button>
                     </Col>
                     <Col md={3} align="right">
-                        <Button color="success" style={{width: '30vh', margin: 0}}
-                                onClick={() => navigate("/register")}>Save</Button>
+                        <Button color={formData?.mechanicServiceId ? "warning" : "success"} style={{width: '30vh', marginLeft: "15px"}}
+                                onClick={mechanicServiceSave}>{formData?.mechanicServiceId ? 'Update' : 'Save'}</Button>
                     </Col>
                 </Row>
 
@@ -183,14 +256,14 @@ const ManageMechanicService = () => {
 
                     <Col md={1} align="right">
                         <Button color="danger" style={{width: '12vh', marginLeft: "0",marginTop:"10px"}}
-                                onClick={() => {
-                                    setFilter(initialFilterState);
-                                    onFilter();
+                                onClick={async () => {
+                                    await setFilter({...initialFilterState});
+                                    await onFilter(true);
                                 }}>Clear</Button>
                     </Col>
                     <Col md={1} align="right">
                         <Button color="success" style={{width: '12vh', marginLeft: "0",marginTop:"10px"}}
-                                onClick={onFilter}>Filter</Button>
+                                onClick={()=>onFilter(false)}>Filter</Button>
                     </Col>
 
                     <Row style={{
@@ -202,7 +275,7 @@ const ManageMechanicService = () => {
                     }}>
                         <Col md={12} style={{padding:0,margin:0}} >
                             <DataTable
-                                columns={columns}
+                                columns={columns(mechanicServiceEdit)}
                                 data={tableData}
                                 pagination
                                 customStyles={customStyles}
